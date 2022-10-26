9546845944b7919a94d712674f4fb300c7c277b3
Aciem22 <gustavoaciem@gmail.com>
2022-10-26 13:13:53 -0300

    subindo todo projeto finalizado

---
+++ projeto-estacionamento/Estacionamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_estacionamento
+{
+    internal class Estacionamento
+    {
+        private decimal precoHora = 15;
+        private decimal Hora, PrecoTotal;
+        List<string> ListaVeiculos = new List<string>();
+
+    public void AdicionarVeiculo(String placa)
+        {
+            if (ListaVeiculos.Contains(placa))
+            {
+                Console.WriteLine("Placa já cadastrada no sistema!");
+            }
+            else
+            {
+                ListaVeiculos.Add(placa);
+                Console.WriteLine($"A placa {placa} foi adicionada no sistema");
+
+            }
+        }
+
+    public void RemoverVeiculo(String placaremove)
+        {
+            if (ListaVeiculos.Contains(placaremove))
+            {
+                Console.WriteLine("Quanto tempo o veículo ficou estacionado?");
+                Hora = Decimal.Parse(Console.ReadLine());
+
+                PrecoTotal = Hora * precoHora;
+
+                Console.WriteLine($"O cliente deverá pagar {PrecoTotal}R$");
+
+                ListaVeiculos.Remove(placaremove);
+                Console.WriteLine($"O veículo da placa {placaremove} foi removido do sistema");
+                
+            }
+            else
+            {
+                Console.WriteLine("Placa do veículo não encontrada!\nTente de novo");
+            }
+            
+        }
+
+    public void ListarVeiculos()
+        {
+            int contador = 0;
+            foreach (String item in ListaVeiculos)
+            {
+                Console.WriteLine($"Posição Nº{contador+1} - {item}");
+                contador++;
+            }
+        }
+
+    }
+}
---
+++ projeto-estacionamento/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_estacionamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string op;
+            string oploop;
+            string opupper = "S";
+
+
+            Estacionamento e = new Estacionamento();
+            while (opupper != "N")
+            {
+                Console.WriteLine("Qual opção irá fazer? \n1-Cadastrar veículo\n2-Remover veículo\n3-Listar veículo\n4-Encerrar programa");
+                op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "1":
+                       
+                            Console.WriteLine("Qual a placa do veículo?");
+                            string placa = Console.ReadLine();
+
+                            
+                            e.AdicionarVeiculo(placa);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Qual a placa do veículo que irá remover?");
+                        string placaremove = Console.ReadLine();
+
+                        e.RemoverVeiculo(placaremove);
+                        break;
+
+                    case "3":
+                        e.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Programa encerrado.");
+                        Environment.Exit(0);
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Opção inválida, digite novamente.");
+                        break;
+                }
+
+                Console.WriteLine("\nDeseja voltar ao menu?\nDigite 's' - sim ou 'n' - não");
+            oploop = Console.ReadLine();
+            // Deixa todos caracteres maiusculos.
+            opupper = oploop.ToUpper();
+
+                
+            }
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
